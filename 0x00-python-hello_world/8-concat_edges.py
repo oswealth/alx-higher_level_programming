90d1c175df0f9082a37305763ef4d6573c1e31c1
oswealth <osbertjaywealth@gmail.com>
2023-10-30 13:12:01 -0700

    Solution to task 8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ langauage that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
