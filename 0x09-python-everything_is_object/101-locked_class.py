6000527a2367e60b1cff1e9498704ba401c4f738
Osbert  D.  Amedzorme <132359218+oswealth@users.noreply.github.com>
2023-11-29 12:49:32 +0000

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
-
-class LockedClass:
-    """
-    Only allows instatiation of an attribute called first_name
-    """
-    __slots__ = ("first_name",)
+#!/usr/bin/python3 
+ """This defines a locked class""" 
+  
+  
+ class LockedClass: 
+  
+     __slots__ = ["first_name"]
