b947124b062e67de253a9b959ce87699015573c8
oswealth <osbertjaywealth@gmail.com>
2024-02-13 18:03:05 -0800

    Rectangle #1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/**
+ * This defines a Rectangle that takes 2 arguments w and h.
+ */
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
