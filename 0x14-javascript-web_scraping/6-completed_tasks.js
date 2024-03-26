947535ea06b76344e24ba9ece2c4b0d8a328df39
oswealth <osbertjaywealth@gmail.com>
2024-03-26 13:59:14 -0700

    How many completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/* computes the number of tasks completed by user id.
+    - the first argument is the API URL.
+    - only prints users with completed task.
+*/
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(tasksCompleted);
+});
