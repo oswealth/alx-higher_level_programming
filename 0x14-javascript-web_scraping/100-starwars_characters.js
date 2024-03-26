ecb6bed47a07fc0e446e7da7cbd599632c0bc0d1
oswealth <osbertjaywealth@gmail.com>
2024-03-26 14:02:21 -0700

    Who was playing in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+/* prints all characters of a Star Wars movie.
+    - the first argument is the Movie ID.
+    - displays one character name by line.
+*/
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  const characters = data.characters;
+  for (const character of characters) {
+    request(character, (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  }
+});
