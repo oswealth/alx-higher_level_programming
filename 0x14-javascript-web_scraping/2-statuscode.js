2925b7c3bf9f7ee85691805ee053a1deb840bc55
oswealth <osbertjaywealth@gmail.com>
2024-03-26 13:48:33 -0700

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/* displays the status code of a GET request.
+    - the first argument is the URL to request (GET).
+    - output format: 'code: <status code>'.
+*/
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
+
